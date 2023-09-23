f467b112073154541402dba99fc7ef6e3e5fd485
loancv12 <loancv12@gmail.com>
2023-09-23 16:09:33 +0700

    fix refresh page

--- server.js
+++ server.js
@@ -25,15 +25,13 @@ app.use(cookieParser());
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname, "build")));
-
 app.use("/", require("./routes/root"));
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/users", require("./routes/userRoutes"));
 app.use("/notes", require("./routes/noteRoutes"));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "./build/index.html"));
 });
 
 app.all("*", (req, res) => {
